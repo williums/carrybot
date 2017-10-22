e9677bc51d4f361254b0433b6a7588af32e371dc
Brandon Williams <bwei.williams@gmail.com>
2017-10-21 22:38:27 -0600

    cleanup

--- events/message.js
+++ events/message.js
@@ -4,6 +4,9 @@ module.exports = message => {
   if (message.author.bot) return;
   if (!message.content.startsWith(settings.PREFIX)) return;
 
+  // ignore messages from other channels
+  if (client.textChannel && message.channel != client.textChannel.name) return;
+  
   const command = message.content.split(' ')[0].slice(settings.PREFIX.length);
   const params = message.content.split(' ').slice(1);
   let cmd;
--- events/ready.js
+++ events/ready.js
@@ -1,18 +1,16 @@
 const chalk = require('chalk');
 const settings = require('../settings.json');
 
-let server, textChannel;
-
 const findTextChannel = chn => {
   return chn.name === settings.TEXTCHANNEL && chn.type === 'text';
 };
 
 module.exports = client => {
-  server = client.guilds.find('name', settings.SERVER);
-  if(server === null) throw `Couldn't find server '${settings.SERVER}'`;
+  client.server = client.guilds.find('name', settings.SERVER);
+  if (client.server === null) throw `Couldn't find server '${settings.SERVER}'`;
   console.log(chalk.bgGreen(`Connected to server '${settings.SERVER}'`));
 
-  textChannel = server.channels.find(findTextChannel); 
-  if(textChannel === null) throw `Couldn't find text channel ${settings.TEXTCHANNEL} in server ${settings.SERVER}`;
-  console.log(chalk.bgGreen(`Found text channel '${settings.TEXTCHANNEL}'`));
+  client.textChannel = client.server.channels.find(findTextChannel); 
+  if (client.textChannel === null) chalk.bgGreen(`Couldn't find text channel '${settings.TEXTCHANNEL}'. Listening to all text channels instead.`);
+  console.log(chalk.bgGreen(`Listening to text channel '${settings.TEXTCHANNEL}'`));
 };
