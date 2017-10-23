80b95a446335108e22545b847097d3a2e28c3055
Brandon Williams <bwei.williams@gmail.com>
2017-10-22 20:08:48 -0600

    restructured party object to use leaders as keys

--- commands/create.js
+++ commands/create.js
@@ -5,30 +5,34 @@ exports.run = function(client, message, args) {
   if (!args[0]) return message.reply(`Usage: ${exports.help.usage}`, {code:'asciidoc'});
   
   // check existing parties
-  for (let p of client.parties) {
-    if (p.leader == message.author) {
-      return message.channel.send(`User ${message.author} is already a leader of a party for boss <${p.boss}>`);
-    }
+  // for (let p of client.parties) {
+  //   if (p.leader == message.author) {
+  //     return message.channel.send(`User ${message.author} is already a leader of a party for boss <${p.boss}>`);
+  //   }
+  // }
+  if (client.parties.hasOwnProperty(message.author)) {
+    return message.channel.send(`User ${message.author} is already a leader of a party for boss <${client.parties[message.author].boss}>`);
   }
 
   // create and push new party
+  let leader = message.author;
   let party = {
-    leader: message.author,
     boss: args[0],
-    members: [message.author],
+    leader: leader,
+    members: [leader],
     comments: ''
   };
   if (args[1]) {
     party.comments = args.slice(1).join(' ');
   }
-  client.parties.push(party);
+  client.parties[leader] = party;
 
   const embed = new Discord.RichEmbed()
     .setColor(0x00AE86)
     .setTitle(`Boss: ${party.boss}`)
     .setDescription(`Comments: ${party.comments ? party.comments : 'None'}`)
-    .addField('Members', party.members[0])
     .addField('Leader', party.leader)
+    .addField('Members', party.members[0])
   return message.channel.send({embed});
 };
 
--- commands/dissolve.js
+++ commands/dissolve.js
@@ -1,12 +1,17 @@
 exports.run = function(client, message) {
   
-  for (let p of client.parties) {
-    if (p.leader == message.author) {
-      let index = client.parties.indexOf(p);
-      client.parties.splice(index, 1);
-      console.log(client.parties);
-      return message.channel.send(`Dissolved party for boss <${p.boss}>`);
-    }
+  // for (let p of client.parties) {
+  //   if (p.leader == message.author) {
+  //     let index = client.parties.indexOf(p);
+  //     client.parties.splice(index, 1);
+  //     console.log(client.parties);
+  //     return message.channel.send(`Dissolved party for boss <${p.boss}>`);
+  //   }
+  // }
+  if (client.parties.hasOwnProperty(message.author)) {
+    let boss = client.parties[message.author].boss;
+    delete client.parties[message.author];
+    return message.channel.send(`Dissolved party for boss <${boss}>`);
   }
 
   return message.channel.send(`User ${message.author} is not a party leader.`);
