2bf6bfd7b29b8e3ae3e289e94f2f409ec4d07035
Brandon Williams <bwei.williams@gmail.com>
2017-10-22 18:49:13 -0600

    create parties

--- commands/create.js
+++ commands/create.js
@@ -1,28 +1,39 @@
+const Discord = require('discord.js');
+
 exports.run = function(client, message, args) {
   
   if (!args[0]) return message.reply(`Usage: ${exports.help.usage}`, {code:'asciidoc'});
   
+  // check existing parties
+  for (let p of client.parties) {
+    if (p.leader == message.author) {
+      return message.channel.send(`User ${message.author} is already a leader of a party for <${p.boss}>`);
+    }
+  }
+
+  // create and push new party
   let party = {
-    leader: `${message.author}`,
-    boss: `${args[0]}`,
-    members: [`${message.author}`],
+    leader: message.author,
+    boss: args[0],
+    members: [message.author],
     comments: ''
   };
   if (args[1]) {
     party.comments = args.slice(1).join(' ');
   }
-  
+  client.parties.push(party);
+
   const embed = new Discord.RichEmbed()
     .setColor(0x00AE86)
     .setTitle(party.boss)
-    .setDescription(party.members[0])
-    .addField('Leader', `${party.leader}`)
-    .addField('Comments', party.comments ? party.comments : 'None')
+    .setDescription(`Comments: ${party.comments ? party.comments : 'None'}`)
+    .addField('Members', party.members[0])
+    .addField('Leader', party.leader)
   return message.channel.send({embed});
 };
 
 exports.help = {
   name: 'create',
-  description: 'Create a new party with the user set as leader. The first and mandatory argument is the target boss, and any additional arguments following [boss_name] are treated as comments.',
+  description: 'Create a new party with the user set as leader. The first argument is the target boss, and any additional arguments are treated as comments.',
   usage: 'create [boss_name] [other comments]'
 };
--- events/message.js
+++ events/message.js
@@ -5,8 +5,9 @@ module.exports = message => {
   if (!message.content.startsWith(settings.PREFIX)) return;
 
   // ignore messages from other channels
-  if (client.textChannel && message.channel != client.textChannel.name) return;
+  if (client.textChannel && message.channel != client.textChannel) return;
   
+  // slice up arguments and invoke command
   const command = message.content.split(' ')[0].slice(settings.PREFIX.length);
   const args = message.content.split(' ').slice(1);
   let cmd;
