9e3143a765f0bc1c7a8fa7335012a228533bbcf3
Brandon Williams <bwei.williams@gmail.com>
2017-11-11 21:27:46 -0700

    cannot kick yourself

--- commands/kick.js
+++ commands/kick.js
@@ -2,17 +2,20 @@ exports.run = function(client, message, args) {
 
   if (!args[0]) return message.reply(`Usage: ${exports.help.usage}`, {code:'asciidoc'});
 
-  // make sure user to be added is valid
   let user = message.mentions.users.first();
-  if (!user) return message.channel.send(`Cannot find user ${args[0]}.`);  
+
+  if (!user) return message.channel.send(`Cannot find user ${args[0]}.`);
+  if (user == message.author) return message.channel.send('You cannot remove yourself from your own party.');
 
   if (client.parties.hasOwnProperty(message.author)) {
 
+    // check if user is in the party
     let index = client.parties[message.author].members.indexOf(user);
     if (index < 0) {
       return message.channel.send(`User ${message.author} is not in ${message.author}'s party.`);   
     }
 
+    // remove user
     client.parties[message.author].members.splice(index, 1);
     return message.channel.send(`User ${user} has been removed from ${message.author}'s party.`);
   }
